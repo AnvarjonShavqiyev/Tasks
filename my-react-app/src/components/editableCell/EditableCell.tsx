6048dcbd60e9b14b8ba598698aac25ffd3227b87
Anvarjon Shavqiyev <129954447+AnvarjonShavqiyev@users.noreply.github.com>
2024-06-28 15:06:56 +0500

    Add files via upload

---
+++ my-react-app/src/components/editableCell/EditableCell.tsx
@@ -0,0 +1,45 @@
+import { Form, Input, InputNumber } from "antd";
+import { User } from "../../types";
+
+interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
+  editing: boolean;
+  dataIndex: string;
+  title: any;
+  inputType: 'number' | 'text';
+  record: User;
+  index: number;
+}
+
+export const EditableCell: React.FC<React.PropsWithChildren<EditableCellProps>> = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
+
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item  
+          name={dataIndex}
+          style={{ margin: 0 }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
