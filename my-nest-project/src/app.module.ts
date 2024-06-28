ab20c266f46cb28ea7046ef852513cf9e15c389a
Anvarjon Shavqiyev <129954447+AnvarjonShavqiyev@users.noreply.github.com>
2024-06-28 14:09:29 +0500

    Database switched to cloud database

--- my-nest-project/src/app.module.ts
+++ my-nest-project/src/app.module.ts
@@ -6,14 +6,11 @@ import { UserController } from './controllers/user.controller';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type:'postgres',
-      host:'localhost',
-      port:5432,
-      username:'postgres',
-      password:'root',
-      database:'Users',
-      entities:[User],
-      synchronize: true
+      type: 'postgres',
+      url: "postgres://default:ZnU1ztG6ocdg@ep-white-snow-a47lu56s-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+      ssl: { rejectUnauthorized: false }, 
+      entities: [User],
+      synchronize: true,
     }),
     TypeOrmModule.forFeature([User]),
   ],
